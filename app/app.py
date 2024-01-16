67dbd8386eef577e086cf5f752924eb2debb4271
root <root@DESKTOP-MOC38TV>
2024-01-16 20:02:05 +0100

    register in json befor yo go to db

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """starting flask"""
+
 import os
+import json
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
@@ -15,6 +17,9 @@ db_config = {
     'database': 'island'
 }
 
+# path to mester json
+json_file_path = os.path.join(os.path.dirname(__file__), 'register_data.json')
+
 @app.route('/')
 def index():
     return render_template('register.html')
@@ -24,6 +29,18 @@ def register():
     username = request.form['username']
     email = request.form['email']
     password = request.form['password']
+
+    #create a dictionary for mister json
+    registration_data = {
+            'username' : username,
+            'email' : email,
+            'password' : password
+            }
+
+    #save the register_data in mester json
+    with open(json_file_path, 'a') as json_file:
+        json.dump(registration_data, json_file)
+        json_file.write('\n')
 
     conn = mysql.connector.connect(**db_config)
     cursor = conn.cursor()
