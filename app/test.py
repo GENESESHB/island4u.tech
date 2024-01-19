a7aa00c3627867f1be2cf707f9e9285aa3f5660d
Hassan BOUDRAA <hassan.hbmama@gmail.com>
2024-01-19 12:09:37 -0800

    add

--- app/test.py
+++ app/test.py
@@ -17,12 +17,11 @@ db_config = {
     'database': 'island'
 }
 
-# path to master json
+# Path to master json
 json_file_path = os.path.join(os.path.dirname(__file__), 'register_data.json')
 
-# Function to check if user exists
+# Check if user exists
 def user_exists(email, password):
-    # Check in the JSON file
     with open(json_file_path, 'r') as json_file:
         for line in json_file:
             try:
@@ -31,54 +30,38 @@ def user_exists(email, password):
                     return True
             except json.decoder.JSONDecodeError:
                 pass
-    
-    # Check in the database
-    conn = mysql.connector.connect(**db_config)
-    cursor = conn.cursor()
-
-    try:
-        cursor.execute("SELECT * FROM users WHERE email = %s", (email,))
-        result = cursor.fetchone()
-        if result and check_password_hash(result[2], password):
-            return True
-    except Exception as e:
-        print(f'Error checking user in database: {e}')
-    finally:
-        cursor.close()
-        conn.close()
-
     return False
 
-# Route for the home page
+# Write to JSON file only if user doesn't exist
+def write_to_json(data):
+    if not user_exists(data['email'], data['password']):
+        with open(json_file_path, 'a') as json_file:
+            json.dump(data, json_file)
+            json_file.write('\n')
+
 @app.route('/')
 def index():
     return render_template('register.html')
 
-# Route for handling registration
 @app.route('/register', methods=['POST'])
 def register():
     username = request.form['username']
     email = request.form['email']
     password = request.form['password']
 
-    # Check if user exists in the database or JSON file
     if user_exists(email, password):
         return render_template('index.html')
 
-    # If the user doesn't exist, proceed with registration
-
-    # Save the registration data in the master JSON file
+    # Create a dictionary for master json
     registration_data = {
         'username': username,
         'email': email,
         'password': password
     }
 
-    with open(json_file_path, 'a') as json_file:
-        json.dump(registration_data, json_file)
-        json_file.write('\n')
+    # Save the registration_data in master json only if the user doesn't exist
+    write_to_json(registration_data)
 
-    # Insert the user into the database
     conn = mysql.connector.connect(**db_config)
     cursor = conn.cursor()
 
@@ -96,7 +79,6 @@ def register():
         cursor.close()
         conn.close()
 
-# Run the application
 if __name__ == '__main__':
     app.run(debug=True)
 
