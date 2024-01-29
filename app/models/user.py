1572d4f709b253e0f8b4b8463cd9bef44d805793
root <root@DESKTOP-MOC38TV>
2024-01-29 19:25:59 +0100

    remove it  its not works

--- app/models/user.py
+++ app/models/user.py
@@ -3,7 +3,7 @@
 import uuid
 
 class User:
-    def __init__(self, username, email, password=None, user_id=None):
+    def __init__(self, user_id, username, email, password=None):
         self.user_id = user_id if user_id else str(uuid.uuid4())
         self.username = username
         self.email = email
--- flask_auth_app/project/main.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-...
-@main.route('/')
-def index():
-    return render_template('index.html')
-
-@main.route('/profile')
-def profile():
-    return render_template('profile.html')
