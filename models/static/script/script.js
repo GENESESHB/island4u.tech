975c83b16a46d4a1d3c0f008a51b3afb65288d56
root <root@DESKTOP-MOC38TV>
2024-03-01 22:34:41 +0100

    add new style

--- models/static/script/script.js
+++ models/static/script/script.js
@@ -15,3 +15,21 @@ function closeModal(modalId) {
     const modal = document.getElementById(modalId);
     modal.style.display = 'none';
 }
+
+function displayImage(previewId, input) {
+    const preview = document.getElementById(previewId);
+    const file = input.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            preview.src = e.target.result;
+            preview.style.display = 'block';
+        };
+        reader.readAsDataURL(file);
+    } else {
+        preview.src = '';
+        preview.style.display = 'none';
+    }
+}
+
